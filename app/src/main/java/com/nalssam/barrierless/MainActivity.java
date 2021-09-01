ea73bb349933040999a8e2b0a1a71100691820d8
CeylonPark <ceylonp20@gmail.com>
2021-09-01 22:27:57 +0900

    feat: naver map api my location

--- app/src/main/java/com/nalssam/barrierless/MainActivity.java
+++ app/src/main/java/com/nalssam/barrierless/MainActivity.java
@@ -1,13 +1,51 @@
 package com.nalssam.barrierless;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.naver.maps.map.*;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+        System.out.println(locationSource.isActivated());
+        System.out.println(locationSource.getLastLocation());
+
+        MapView mapView = findViewById(R.id.map);
+        mapView.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setCompassEnabled(false);
+        uiSettings.setLogoClickEnabled(false);
+        uiSettings.setLocationButtonEnabled(true);
     }
 }
