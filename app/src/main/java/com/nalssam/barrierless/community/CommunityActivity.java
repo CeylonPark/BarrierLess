f13af91b8d69285f074dbf48d74cfe25fa790e40
CeylonPark <ceylonp20@gmail.com>
2021-09-05 22:25:09 +0900

    feat: report popup activuty

---
+++ app/src/main/java/com/nalssam/barrierless/community/CommunityActivity.java
@@ -0,0 +1,22 @@
+package com.nalssam.barrierless.community;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageButton;
+import androidx.annotation.Nullable;
+import com.nalssam.barrierless.R;
+
+public class CommunityActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.report_activity);
+
+        ImageButton cancelBtn = findViewById(R.id.reportCancelBtn);
+        cancelBtn.setOnClickListener(view -> finish());
+    }
+}
--- app/src/main/java/com/nalssam/barrierless/community/CommunityView.java
+++ app/src/main/java/com/nalssam/barrierless/community/CommunityView.java
@@ -1,30 +1,53 @@
 package com.nalssam.barrierless.community;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import com.nalssam.barrierless.MainActivity;
 import com.nalssam.barrierless.R;
 import com.nalssam.barrierless.view.ViewState;
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.MarkerIcons;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommunityView implements ViewState {
-    private List<Marker> markers = new ArrayList<>();
+    private static final int ADD = 0;
+    private static final int PROBLEM = 1;
+    private final List<Marker> markers = new ArrayList<>();
+
+    private void onClickReport(MainActivity mainActivity, int type) {
+        Intent intent = new Intent(mainActivity, CommunityActivity.class);
+        intent.putExtra("data", "Test Popup");
+
+        mainActivity.startActivity(intent);
+    }
 
     @Override
     public void onOpen(MainActivity mainActivity) {
         //제보 버튼 활성화
-        mainActivity.findViewById(R.id.reportNew).setVisibility(View.VISIBLE);
-        mainActivity.findViewById(R.id.reportProblem).setVisibility(View.VISIBLE);
+        ImageButton reportNewBtn = mainActivity.findViewById(R.id.reportNew);
+        reportNewBtn.setVisibility(View.VISIBLE);
+        reportNewBtn.setOnClickListener(view -> onClickReport(mainActivity, CommunityView.ADD));
+
+        ImageButton reportProblemBtn = mainActivity.findViewById(R.id.reportProblem);
+        reportProblemBtn.setVisibility(View.VISIBLE);
+        reportProblemBtn.setOnClickListener(view -> onClickReport(mainActivity, CommunityView.PROBLEM));
 
         //마커 테스트
         Marker marker = new Marker();
         markers.add(marker);
-        marker.setPosition(new LatLng(36.38567474070798, 127.30387532269494));
-        marker.setMaxZoom(16);
+        marker.setPosition(new LatLng(36.3906994443793, 127.30733252302068));
         marker.setMap(mainActivity.getNaverMap());
+
+        Marker marker2 = new Marker();
+        markers.add(marker2);
+        marker2.setPosition(new LatLng(36.39064492429601, 127.30882718744387));
+        marker2.setIcon(MarkerIcons.PINK);
+        marker2.setMap(mainActivity.getNaverMap());
     }
 
     @Override
